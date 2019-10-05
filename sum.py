3250a094e6a52fbb02421a72609fe8fa6ad3c44a
drvalerioh <valerioh242@gmail.com>
2019-10-05 16:43:17 +0300

    Python program to sum two integers

---
+++ sum.py
@@ -0,0 +1,12 @@
+def my_function():
+
+  x=int(input('enter x: '))
+  y=int(input('enter y: '))
+  total= x +y
+  if total>=50:
+    return "above 50"
+  else:
+    return "below 50"
+
+
+print(my_function())
